61818982163493b8214e11f093834e6ab6372c37
suraj.agariya@ext.religare.in <suraj.agariya@ext.religare.in>
2022-07-10 06:58:11 +0530

    "NG if done"

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BookComponent implements OnInit {
  name:string="A"
  author:string="Tom Martin"
- disabled:boolean = false;
+ isShowBooks:boolean = false;
  src="https://images-eu.ssl-images-amazon.com/images/I/41zoxjP9lcL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg"
 
  name1:string="A"
@@ -17,7 +17,7 @@ export class BookComponent implements OnInit {
 
   constructor() { }
 
-  name3:string="";
+  
 
   ngOnInit(): void {
   }
