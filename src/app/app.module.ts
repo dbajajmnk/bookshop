996aa238c553cb4cf2d7b73451f046687a27b47a
suraj.agariya@ext.religare.in <suraj.agariya@ext.religare.in>
2022-07-11 06:55:14 +0530

    "Rounting introduction done"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppCompontnet } from "./app.compontent";
+import { AppRoutingModule } from "./app.routing.module";
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
     declarations:[AppCompontnet, CartComponent],
-    imports:[BrowserModule,FormsModule,BooksModule],
+    imports:[BrowserModule,FormsModule,BooksModule,AppRoutingModule],
     bootstrap:[AppCompontnet]
     
 })
